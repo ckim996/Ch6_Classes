aee0a11a1c59ae20a706b115071cf2e2692428a3
Chanwoo Kim <chanwoo_kim96@hotmail.com>
2021-06-28 16:27:26 -0700

    6.02

--- src/ChoHan.java
+++ src/ChoHan.java
@@ -19,6 +19,8 @@ public class ChoHan
 
         for(int i = 0; i < MAX_ROUNDS; i++)
         {
+            System.out.println("--------------------------");
+            System.out.println("ROUND " + (i+1));
             System.out.println("");
             System.out.println("Dealer will now roll the dice");
             d.rollDice();
@@ -34,11 +36,44 @@ public class ChoHan
     public static void roundResults(Dealer dealer, Player p1, Player p2)
     {
         System.out.println("The dealer got the following two dice: " + dealer.getD1Value() + " " + dealer.getD2Value());
-        System.out.println("Result: ");
+        System.out.println("Result: " + dealer.getChoOrHan());
+
+        checkGuess(p1,dealer);
+        checkGuess(p2,dealer);
+
+        System.out.println("");
+
+    }
+
+    public static void checkGuess(Player p, Dealer d)
+    {
+        final int POINTS = 1;
+        String pGuess = p.getGuess();
+        String dGuess = d.getChoOrHan();
+
+        System.out.printf("Player %s guessed %s.\n",p.getName(),pGuess);
+        //System.out.printf("Dealer guessed %s", dGuess);
+
+        if(pGuess.equalsIgnoreCase(dGuess))
+        {
+            p.addPoints(POINTS);
+            System.out.printf("Awarding %d point(s) to %s.\n", POINTS,p.getName());
+        }
     }
 
     public static void displayGrandWinner(Player p1, Player p2)
     {
+        System.out.println("-----------------------------------");
+        System.out.println("GAME IS OVER!");
+        System.out.printf("TOTAL POINTS:\n%s: %d\n%s: %d\n", p1.getName(), p1.getPoints(), p2.getName(),p2.getPoints());
 
+        if(p1.getPoints() > p2.getPoints())
+        {
+            System.out.println(p1.getName() + " is the winner!");
+        }else if(p1.getPoints() < p2.getPoints()){
+            System.out.println(p2.getName() + " is the winner!");
+        }else{
+            System.out.println("BOTH TIE!");
+        }
     }
 }
